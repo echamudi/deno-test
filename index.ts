53f8f84c30a064196610645b61fbcf733f8ae526
Ezzat Chamudi <echamudi@outlook.com>
2021-01-14 02:52:19 +0300

    update

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-const text = await Deno.readTextFile("./food.json");
+const text = await Deno.readTextFile(new URL("food.json", import.meta.url));
 
+console.log(new URL("food.json", import.meta.url));
 console.log(JSON.parse(text));
 
 export { text };
